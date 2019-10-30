ea4e23346535ca9f436aa04a253d72e0fde3361e
Unknown <cleytonbritto3003@gmail.com>
2019-10-30 14:05:08 -0300

    carrossel

--- js/carrossel.js
+++ js/carrossel.js
@@ -6,12 +6,14 @@
 			$('#volta-slide').css({display: "flex"});
 			$('#passa-pg').css({display: "flex"});
 			$('#volta-pg').css({display: "none"});
-		}else if($('.carousel-item#ultimo').hasClass('active')){
+		}
+		if($('.carousel-item#ultimo').hasClass('active')){
 			$('#passa-slide').css({display: "flex"});
 			$('#volta-slide').css({display: "none"});
 			$('#passa-pg').css({display: "none"});
 			$('#volta-pg').css({display: "flex"});
-		}else{
+		}
+		if(!$('.carousel-item#primeiro').hasClass('active') && !$('.carousel-item#ultimo').hasClass('active')){
 			$('#passa-slide').css({display: "flex"});
 			$('#volta-slide').css({display: "flex"});
 			$('#passa-pg').css({display: "none"});
