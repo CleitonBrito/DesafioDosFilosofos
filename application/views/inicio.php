88016908a190734eeaf4f658d8d62a4a34117c87
Unknown <cleytonbritto3003@gmail.com>
2019-10-30 11:14:33 -0300

    update

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -4,7 +4,7 @@
 	<title>Projeto SO</title>
 	<link rel="stylesheet" type="text/css" href="<?= base_url('css/inicio.css'); ?>">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.0.min.js"></script>
+	<script type="text/javascript" src="<?= base_url('js/jquery-1.10.0.min.js'); ?>"></script>
 </head>
 <body>
 	<div id="fundo" class="container">
