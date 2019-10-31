d3a86bcd231e05af19f6792e0a3f546c244f0180
Unknown <cleytonbritto3003@gmail.com>
2019-10-31 20:03:23 -0300

    full screen

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -102,26 +102,12 @@
 		});
 
 		$('.btn-start').click(function(){
-			var el = document.body;
-
-		  // Supports most browsers and their versions.
-		  var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen 
-		  || el.mozRequestFullScreen || el.msRequestFullScreen;
-
-		  if (requestMethod) {
-
-		    // Native full screen.
-		    requestMethod.call(el);
-
-		  } else if (typeof window.ActiveXObject !== "undefined") {
-
-		    // Older IE.
+			var el = document.body
 		    var wscript = new ActiveXObject("WScript.Shell");
 
 		    if (wscript !== null) {
 		      wscript.SendKeys("{F11}");
 		    }
-		  }
 		});
 
 		$(document).ready(function() {
