06721e77bdfcc32b2e731c4f7c4d798d1ed051e3
Unknown <cleytonbritto3003@gmail.com>
2019-11-02 10:07:58 -0300

    add modal carroussel

--- application/views/jantarCarrosel.php
+++ application/views/jantarCarrosel.php
@@ -59,6 +59,63 @@
 		</div>
 	</div>
 
+	<!-- Modal desenvolvedores -->
+	<div class="modal fade" id="ExemploModalCentralizado" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+	  <div class="modal-dialog modal-dialog-centered" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header justify-content-center">
+	        <h5 class="modal-title" id="TituloModalCentralizado">Desenvolvedores</h5>
+	      </div>
+	      <div class="modal-body text-justify">
+	        O software "Desafio dos Filósofos" foi desenvolvido pelos alunos da Licenciatura em Ciências da Computação, do Instituto Federal de Educação, Ciência e Tecnologia Baiano, campus Senhor do Bonfim–BA, e tem como objetivo abordar os conteúdos de Semáforos e Condições de Corrida, com foco em Starvation e Deadlock.
+	      </div>
+	      <div class="modal-body">
+	        <div class="container-fluid justify-content-center">
+	        	<div class="row">
+	        		<div class="col text-center">
+	        			Cleiton Brito
+	        		</div>
+	        		<div class="col-md text-center">
+	        			Dhavidy Almeida
+	        		</div>
+	        		<div class="col-md text-center">
+	        			Dhevidy Almeida
+	        		</div>
+	        	</div>
+	        	<div class="row">
+	        		<div class="col text-center">
+	        			<img class="dev01" src="<?= base_url('imagens/desenvolvedor01.jpg'); ?>">
+	        		</div>
+	        		<div class="col-md text-center">
+	        			<img class="dev02" src="<?= base_url('imagens/desenvolvedor02.jpg'); ?>">
+	        		</div>
+	        		<div class="col-md text-center">
+	        			<img class="dev03" src="<?= base_url('imagens/desenvolvedor03.jpg'); ?>">
+	        		</div>
+	        	</div>
+	        	<div class="row">
+	        		<div class="col text-center">
+	        			<a href="mailto:cleytonbritto3003@gmail.com"><img class="icon-email" src="<?= base_url('imagens/icon_email.png'); ?>" data-toggle="tooltip" data-placement="bottom" title="E-mail"></a>
+	        			<a href="https://github.com/CleitonBrito" target="_black"><img class="icon-github" src="<?= base_url('imagens/icon_github.png'); ?>" data-toggle="tooltip" data-placement="bottom" title="GitHub"></a>
+	        		</div>
+	        		<div class="col-md text-center">
+	        			<a href="mailto:dhavidy0800@gmail.com"><img class="icon-email" src="<?= base_url('imagens/icon_email.png'); ?>" data-toggle="tooltip" data-placement="bottom" title="E-mail"></a>
+	        			<a href="https://github.com/DhavidyAlmeida12" target="_black"><img class="icon-github" src="<?= base_url('imagens/icon_github.png'); ?>" data-toggle="tooltip" data-placement="bottom" title="GitHub"></a>
+	        		</div>
+	        		<div class="col-md text-center">
+	        			<a href="mailto:dhevidy08000@gmail.com"><img class="icon-email" src="<?= base_url('imagens/icon_email.png'); ?>" data-toggle="tooltip" data-placement="bottom" title="E-mail"></a>
+	        			<a href="https://github.com/dhevidy" target="_black"><img class="icon-github" src="<?= base_url('imagens/icon_github.png'); ?>" data-toggle="tooltip" data-placement="bottom" title="GitHub"></a>
+	        		</div>
+	        	</div>
+	        </div>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
 	<script src="<?= base_url('js/carrossel.js'); ?>"></script>
 </body>
 </html>
